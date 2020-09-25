0da381273929aa0b092e114bb25b5a4d114fe18e
oripk <cotmd620389@gmail.com>
2020-09-25 11:39:04 +0000

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/')); // redirect bootstrap JS
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/popper.js', express.static(__dirname + '/node_modules/popper.js/dist')); // redirect popper.js
+
+var server = app.listen(3000, function(){
+	console.log("express server on port 3000")
+});
