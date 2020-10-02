61b1ece40fac23abc695b82b238941390f99bd04
oripk <cotme6203@naver.com>
2020-10-02 20:00:59 +0900

    design index

--- router/main.js
+++ router/main.js
@@ -2,4 +2,12 @@ module.exports = function(app){
 	app.get('/', function(req, res){
 		res.render('index.html')
 	});
+	
+	app.get('/contact', (req, res) =>{
+		res.render('contact.html')
+	});
+
+	app.get('/list', (req,res) =>{
+		res.render('list.html')
+	});
 }
